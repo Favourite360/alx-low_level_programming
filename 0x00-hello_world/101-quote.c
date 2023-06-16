d6a5a61c7c10c3fe5e4af5fbbd273d155810417a
Favourite Emmanuel <send2fav@gmail.com>
2023-06-16 15:01:11 -0700

    low level with C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - shall print out the last part of quote in the standard error.
- * Return: 1 as success.
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
 int main(void)
 {
-	fput(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
