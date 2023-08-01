0c045bd5c2122d94cf98e276e26d537382b83a54
Favourite Emmanuel <send2fav@gmail.com>
2023-08-01 11:13:35 -0700

    SOLVED 0x13-More Singly linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list_t list
+ * @head: first element of list.
+ *
+ * Return: no return or NULL.
+ */
+void free_listint2(listint_t **head)
+{
+	struct listint_s *temp = NULL;
+	struct listint_s *aux =  NULL;
+
+	if (head == NULL)
+		return;
+	temp = *head;
+	while (temp != NULL)
+	{
+		aux = temp->next;
+		free(temp);
+		temp = aux;
+	}
+	*head = NULL;
+}
